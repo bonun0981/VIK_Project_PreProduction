e66154b816b7e7b865a9f3f1be8feef40f1d8926
pattarapon sarajit <sarajit_p@silpakorn.edu>
2025-11-18 23:19:28 +0700

    add character controller for player character

---
+++ Projrct/Assets/Assets/C#_Code/Main_Character/PlayerMovement.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Animator playerController;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Transform camTransform;
+
+    [Header("Movement")]
+    [SerializeField] private float originalwalkSpeed = 2f;
+    private float walkSpeed = 2f;
+    [SerializeField] private float runSpeed = 6f;
+    [SerializeField] private float turnSpeed = 10f; // rotation speed
+    [SerializeField] private float gravity = -9.81f;
+
+    private float inputX;
+    private float inputY;
+
+    private Vector3 velocity; // vertical velocity
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        InputManager();
+        MovementMode();
+        GroundMovement();
+        RotateCharacter();
+    }
+
+    void InputManager()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+        
+        
+
+           
+    }
+
+    void GroundMovement()
+    {
+        // Camera relative movement
+        Vector3 camForward = camTransform.forward;
+        Vector3 camRight = camTransform.right;
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 move = camForward * inputY + camRight * inputX;
+        move.Normalize(); // consistent diagonal speed
+
+        // Move character
+        characterController.Move(move * walkSpeed * Time.deltaTime);
+        playerController.SetFloat("Velocity",Mathf.Abs(move.magnitude));
+        // Gravity
+        if (characterController.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void RotateCharacter()
+    {
+        // Only rotate if there is movement input
+        Vector3 move = camTransform.forward * inputY + camTransform.right * inputX;
+        move.y = 0;
+
+        if (move.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    public void MovementMode()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerController.SetBool("isRunning", true);
+            walkSpeed = runSpeed;
+        }
+        else
+        {
+            playerController.SetBool("isRunning", false); 
+            walkSpeed = originalwalkSpeed;
+        }
+    }
+}
