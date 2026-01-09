f2a8790c5a63f83a283bc531dffd501b6e82618f
pattarapon sarajit <sarajit_p@silpakorn.edu>
2026-01-10 01:14:38 +0700

    add target lock / enemy take hit effect
    - lock enemy when press right click and change camera to lock camera
    -when enemy take damage change enemy material to pure white for 0.5f second

--- Projrct/Assets/CameraLock.cs
+++ Projrct/Assets/CameraLock.cs
@@ -1,16 +1,110 @@
+using System.Net;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem.XInput;
+using UnityEngine.Rendering;
+
 
 public class CameraLock : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   [SerializeField] CinemachineCamera freeLookCamera;
+    [SerializeField] CinemachineCamera lockCamera;
+
+
+    public float lockOnRange = 10f;
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] float originalXAisSpeed;
+    [SerializeField] float originalYAisSpeed;
+    private Transform currentTarget;
+    [SerializeField]private Transform lockTarget;
+    private bool isLock;
+    CinemachineInputAxisController playerInputController;
+    CinemachineInputAxisController enemyInputController;
+
+
+
+
+
+
+    public float lookHeight = 1.2f;
+    public float followSpeed = 10f;
+
+    private void Start()
     {
+        playerInputController = freeLookCamera.GetComponent<CinemachineInputAxisController>();
+        enemyInputController = lockCamera.GetComponent<CinemachineInputAxisController>();
+
+    } 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!isLock)
+            {
+                LockTarget();
+            }
+            else
+            {
+                UnlockTarget(); 
+            }
+        }
+        if(isLock && currentTarget==null)
+        {
+            UnlockTarget();
+        }
+
+    }
+    private void LateUpdate()
+    {
+        if (!isLock || currentTarget == null) return;
         
+            Vector3 midPoint = (transform.position + currentTarget.position)*0.5f;
+            midPoint.y += lookHeight;
+
+            lockTarget.position = Vector3.Lerp(
+            lockTarget.position,
+            midPoint,
+            Time.deltaTime * followSpeed);
+        RotateAtEnemy();
+
+    }
+    public void LockTarget()
+    {
+        Collider[] enemies = Physics.OverlapSphere(transform.position,lockOnRange,enemyLayer);
+        if (enemies.Length == 0) return;
+        
+        Debug.Log("Locking Target: " + enemies[0].name);
+        currentTarget = enemies[0].transform;
+        lockCamera.LookAt = lockTarget;
+        playerInputController.enabled = false;
+        enemyInputController.enabled = false;
+        isLock = true;
+        freeLookCamera.Priority = 0;
+        lockCamera.Priority = 10;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UnlockTarget()
     {
-        
+        freeLookCamera.LookAt = transform;
+        playerInputController.enabled = true;
+        enemyInputController.enabled = true;
+        currentTarget = null;
+        isLock = false;
+        freeLookCamera.Priority = 10;
+        lockCamera.Priority = 0;
+    }
+    public void RotateAtEnemy()
+    {
+        if (!isLock || currentTarget == null) return;
+
+        Vector3 dir = currentTarget.position - transform.position;
+        dir.y = 0;
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            Quaternion.LookRotation(dir),
+            Time.deltaTime * 12f
+        );
     }
 }
--- Projrct/Assets/Enemy.cs
+++ Projrct/Assets/Enemy.cs
@@ -1,16 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Material originalMat;
+    [SerializeField] Material damageMat;
+    [SerializeField] MeshRenderer reder;
+    [SerializeField] float flashTime = 0.75f;
+
     [SerializeField] float health = 100f;
     [SerializeField]Rigidbody enemyRigidbody;
     private float knockbackForce = 2.75f;
 
+    private void Start()
+    {
+        
+    }
     public void TakeDamage(float damage,Vector3 playerDirection)
     {
         health -= damage;
         enemyRigidbody.AddForce(playerDirection * knockbackForce, ForceMode.Impulse);
-
+        StartCoroutine(TakeDamageFlash(flashTime)); 
 
 
 
@@ -24,5 +34,12 @@ public class Enemy : MonoBehaviour
     public void Die()
     {
         Destroy(gameObject);
+    }
+
+    IEnumerator TakeDamageFlash(float time)
+    {
+       reder.material=damageMat;
+        yield return new WaitForSeconds(time);
+        reder.material=originalMat;
     }
 }
