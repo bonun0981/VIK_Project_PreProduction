7b95befaf16be2c91dabab11ae4dd6a8a940daed
pattarapon sarajit <bonun0981@gmail.com>
2026-03-23 23:36:49 +0700

    add ui setting

---
+++ src/VikProject/Assets/Assets/C# Scripts/Manager/UiManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField]private GameObject pauseMenu;
+    [SerializeField]private GameObject settingMenu;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // ถ้า Settings เปิดอยู่ → ปิดทั้งหมด
+            if (settingMenu != null && settingMenu.activeInHierarchy)
+            {
+                CloseAllMenus();
+            }
+            else
+            {
+                PauseMenuHandler();
+            }
+        }
+    }
+
+    private void CloseAllMenus()
+    {
+        settingMenu?.SetActive(false);
+        pauseMenu?.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+
+    public void PauseMenuHandler()
+    {
+        if (pauseMenu.activeInHierarchy)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    public void OpenSettingMenu()
+    {
+        settingMenu.SetActive(true);
+    }
+    public void CloseSettingMenu()
+    {
+        settingMenu?.SetActive(false);
+    }
+
+}
